f238db110724cc8ed8cbce6c92e06796b061f45a
Lev Zakharov <l.j.zakharov@gmail.com>
2017-11-18 06:17:10 +0300

    add configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
+
+db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     return app
