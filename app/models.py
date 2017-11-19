01f27afea394d0376066c205a9b69b033f98bc79
Lev Zakharov <l.j.zakharov@gmail.com>
2017-11-19 03:10:52 +0300

    create model for items

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from app import db
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    completed = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Item.query.all()
+
+    def __repr__(self):
+        return '<Item %r>' % (self.name)
